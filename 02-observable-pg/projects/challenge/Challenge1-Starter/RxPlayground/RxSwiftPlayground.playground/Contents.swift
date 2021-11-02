5e9e932f9743ad5e9f5604c08b1dbf7a66db2b9e
Theo Vora <theo.vora@gmail.com>
2021-11-02 16:40:22 -0600

    Complete challenge for do operator

--- 02-observable-pg/projects/challenge/Challenge1-Starter/RxPlayground/RxSwiftPlayground.playground/Contents.swift
+++ 02-observable-pg/projects/challenge/Challenge1-Starter/RxPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -2,20 +2,27 @@ import Foundation
 import RxSwift
 
 example(of: "never") {
-  let observable = Observable<Any>.never()
-  
-  observable
-    .subscribe(
-      onNext: { element in
-        print(element)
-      },
-      onCompleted: {
-        print("Completed")
-      },
-      onDisposed: {
-        print("Disposed")
-      }
-    )
+    let observable = Observable<Any>.never()
+    let disposeBag = DisposeBag()
+    
+    observable
+        .do(
+            onSubscribe: {
+                print("Subscribed!")
+            }
+        )
+        .subscribe(
+            onNext: { element in
+                print(element)
+            },
+            onCompleted: {
+                print("Completed")
+            },
+            onDisposed: {
+                print("Disposed")
+            }
+        )
+        .disposed(by: disposeBag)
 }
 
 /// Copyright (c) 2020 Razeware LLC
